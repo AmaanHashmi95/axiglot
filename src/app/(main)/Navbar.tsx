f0f6479df51b34d2327aefb265e8fb967406ea9a
AmaanHashmi95 <74192972+AmaanHashmi95@users.noreply.github.com>
2024-12-11 12:41:24 +0000

    Header Logo etc

--- src/app/(main)/Navbar.tsx
+++ src/app/(main)/Navbar.tsx
@@ -1,13 +1,15 @@
 import Link from "next/link";
 import UserButton from "@/components/UserButton";
 import SearchField from "@/components/SearchField";
+import Image from "next/image";
+import logo from "@/assets/logo.png";
 
 export default function Navbar() {
     return (
       <header className="sticky top-0 z-10 bg-card shadow-sm">
         <div className="mx-auto flex max-w-7xl flex-wrap items-center justify-center gap-5 px-5 py-3">
-        <Link href="/" className="text-2xl font-bold text-primary">
-        bugbook
+        <Link href="/">
+        <Image src={logo} alt="" width={150} height={50}/>
         </Link>
         <SearchField />
         <UserButton className="sm:ms-auto" />
