00d623250f9a67e0eec4f0fce160cd23d503d877
AmaanHashmi95 <74192972+AmaanHashmi95@users.noreply.github.com>
2025-04-20 11:16:33 +0100

    Text Logo Design

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -13,7 +13,20 @@ export default function Page() {
     <main className="flex h-screen items-center justify-center p-5">
       <div className="flex h-250 max-h-[40rem] w-full max-w-[64rem] overflow-hidden rounded-2xl bg-card shadow-2xl">
         <div className="w-full space-y-10 overflow-y-auto p-10 md:w-1/2">
-          <h1 className="text-center text-3xl font-bold">Login to Axiglot</h1>
+          <h1 className="text-center text-3xl font-bold">
+            Login to{' '}
+            <span className="inline-block">
+              Ax<span className="relative inline-block">
+                i
+                <span className="absolute left-1/2 top-0 -translate-x-1/2 -translate-y-1/3 text-[#00E2FF]">
+                  •
+                </span>
+              </span>
+              <span className="bg-gradient-to-r from-[#ff8a00] to-[#ef2626] bg-clip-text text-transparent">
+                Glot
+              </span>
+            </span>
+          </h1>
           <div className="space-y-5">
             <LoginForm />
             <Link href="/signup" className="block text-center hover:underline">
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -14,7 +14,20 @@ export default function Page() {
         <div className="flex h-300 max-h-[40rem] w-full max-w-[64rem] overflow-hidden rounded-2xl bg-card shadow-2xl">
             <div className="w-full space-y-10 overflow-y-auto p-10 md:w-1/2">
             <div className="space-y-1 text-center">
-            <h1 className="text-3xl font-bold">Sign up to Axiglot</h1>
+            <h1 className="text-center text-3xl font-bold">
+            Sign up to{' '}
+            <span className="inline-block">
+              Ax<span className="relative inline-block">
+                i
+                <span className="absolute left-1/2 top-0 -translate-x-1/2 -translate-y-1/3 text-[#00E2FF]">
+                  •
+                </span>
+              </span>
+              <span className="bg-gradient-to-r from-[#ff8a00] to-[#ef2626] bg-clip-text text-transparent">
+                Glot
+              </span>
+            </span>
+          </h1>
             <p className="text-muted-foreground">
               And access <span className="italic">all</span> of our languages.
             </p>
