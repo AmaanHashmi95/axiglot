2c8cbadff8b489fee089c50a968f895857619780
AmaanHashmi95 <74192972+AmaanHashmi95@users.noreply.github.com>
2025-04-20 10:59:42 +0100

    Login/Sign up Images

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,4 +1,4 @@
-import loginImage from "@/assets/Global.png";
+import loginImage from "@/assets/Hexagon Logo Clear.png";
 import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <main className="flex h-screen items-center justify-center p-5">
-      <div className="flex h-full max-h-[40rem] w-full max-w-[64rem] overflow-hidden rounded-2xl bg-card shadow-2xl">
+      <div className="flex h-250 max-h-[40rem] w-full max-w-[64rem] overflow-hidden rounded-2xl bg-card shadow-2xl">
         <div className="w-full space-y-10 overflow-y-auto p-10 md:w-1/2">
           <h1 className="text-center text-3xl font-bold">Login to Axiglot</h1>
           <div className="space-y-5">
