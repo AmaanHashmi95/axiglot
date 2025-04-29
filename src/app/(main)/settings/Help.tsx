34a95d31870355217bb0e16b8fef827cf20d23ce
AmaanHashmi95 <74192972+AmaanHashmi95@users.noreply.github.com>
2025-04-29 16:08:05 +0100

    Update Help.tsx

--- src/app/(main)/settings/Help.tsx
+++ src/app/(main)/settings/Help.tsx
@@ -1,11 +1,20 @@
 export default function Help() {
   return (
     <div>
+      <div>
+      You can view our Terms & Conditions here{" "}
+      <a href="https://www.axiglot.com/policies.html" className="underline text-primary">
+        Axiglot Policies
+      </a>{" "}
+      .
+      </div>
+      <div>
       If you wish to delete your account, please email{" "}
       <a href="mailto:info@axiglot.com" className="underline text-primary">
         info@axiglot.com
       </a>{" "}
       requesting deletion.
+      </div>
     </div>
   );
 }
